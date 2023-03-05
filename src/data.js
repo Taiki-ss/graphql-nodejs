ae68360fc182b30c635c5f84d94ff7adb85a14ec
Taiki-nk <taikifate.0206@gmail.com>
2023-03-05 15:04:37 +0900

    簡易的サーバー構築

---
+++ src/data.js
@@ -0,0 +1,33 @@
+/* data.js */
+
+const Users = [
+  {
+    id: 1,
+    name: "Nasu Kinoko",
+    email: "kinoko@gmail.com",
+    posts: [
+      {
+        id: 1,
+        title: "まほよの主役は有朱",
+        published: true,
+        author: 1,
+      },
+      {
+        id: 2,
+        title: "月姫リメイク続編はいつになるのか",
+        published: true,
+        author: 1,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Takeuchi Takashi",
+    email: "takeuchi@gmail.com",
+    posts: [],
+  },
+];
+
+module.exports = {
+  Users,
+};
